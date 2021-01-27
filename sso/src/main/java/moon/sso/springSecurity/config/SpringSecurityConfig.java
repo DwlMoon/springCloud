ab0845073562169ef7bf837083932489c05d7453
haha <haha@163.com>
2021-01-27 13:52:24 +0800

    sso接口设定权限

--- sso/src/main/java/moon/sso/springSecurity/config/SpringSecurityConfig.java
+++ sso/src/main/java/moon/sso/springSecurity/config/SpringSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  **/
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -37,10 +39,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure (HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .formLogin().loginPage("/static/haha.html").loginProcessingUrl("/authentication/form").failureForwardUrl("/SecurityController/fail")
+                .formLogin().loginPage("/static/haha.html").loginProcessingUrl("/authentication/form").failureForwardUrl("/sec/fail")
                 .and()
                 .authorizeRequests()
-                .antMatchers("/SecurityController/insertUserData","/SecurityController/fail").permitAll()
+                .antMatchers("/sec/removeToken/**","/sec/fail").permitAll()
                 .antMatchers("/static/**").permitAll()
                 .antMatchers("/oauth/**").permitAll()
                 .anyRequest().authenticated()
--- sso/src/main/java/moon/sso/springSecurity/controller/SecurityController.java
+++ sso/src/main/java/moon/sso/springSecurity/controller/SecurityController.java
@@ -4,10 +4,10 @@ package moon.sso.springSecurity.controller;
 
 import moon.sso.springSecurity.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @program: moon
@@ -16,33 +16,59 @@ import org.springframework.web.bind.annotation.ResponseBody;
  **/
 
 @Controller
-@RequestMapping(value = "SecurityController")
+@RequestMapping(value = "sec")
 public class SecurityController {
 
     @Autowired
     SecurityService securityService;
 
+
+    @Autowired
+    ConsumerTokenServices consumerTokenServices;
+
+
+    /**
+     * 插入用户数据
+     * @return
+     */
     @RequestMapping(value = "insertUserData",method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String insertUserData(){
         securityService.insertUserData();
         return "success";
     }
 
 
+    /**
+     * 清除redis中token
+     * @param accessToken
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/removeToken/{accessToken}",method = RequestMethod.GET)
+    public Boolean removeToken(@PathVariable(value = "accessToken") String accessToken){
+        boolean revokeToken = consumerTokenServices.revokeToken(accessToken);
+        return revokeToken;
+    }
+
+
+
+
     @RequestMapping(value = "test",method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public String test(){
         return "success";
     }
 
+    /**
+     * 验证失败页面
+     * @return
+     */
     @RequestMapping(value = "fail",method = {RequestMethod.GET, RequestMethod.POST})
     public String fail(){
+//        返回登录失败页面
         return "fail";
-    }
-
-    @RequestMapping(value = "login",method = {RequestMethod.GET, RequestMethod.POST})
-    public String login(){
-        return "haha";
     }
 
 
